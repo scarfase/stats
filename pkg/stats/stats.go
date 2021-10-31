0f2a110bdd3a89044142cd3cf0a7c338190a2def
scarfase <scarfase1989@gmail.com>
2021-10-31 16:30:24 +0600

    add CategoriesTotal func and tests

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,6 +1,8 @@
 package stats
 
-import "github.com/scarfase/bank/v2/pkg/types"
+import (
+	"github.com/scarfase/bank/v2/pkg/types"
+)
 
 // Avg рассчитывает среднюю сумму платежа
 func Avg(payments []types.Payment) types.Money {
@@ -27,4 +29,14 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 		}
 	}
 	return sum
+}
+
+// CategoriesTotal возвращает сумму платежей по каждой категории.
+func CategoriesTotal(payments []types.Payment) map[types.Category]types.Money {
+	categories := map[types.Category]types.Money{}
+
+	for _, payment := range payments {
+		categories[payment.Category] += payment.Amount
+	}
+	return categories
 }
